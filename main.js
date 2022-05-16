8c2eab3d8ee8f3e340ae6cae67bcbdf383947668
HaiGre <vth6121@gmail.com>
2022-05-16 23:49:17 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,178 @@
+var products = [
+    {
+        id: 1,
+        name: 'ORIGINAL WAYFARER',
+        page:'./product-detail/product01.html',
+        desc: 'These acetate sunglasses with a rectangular shape have a contemporary frame front characterized by the three-dimensional workmanship and thick lines. The bridge area iconographically reinterprets the historic triangle logo of the fashion house. The wide straight temples are decorated with the Prada lettering logo.',
+        brand: 'Ray Ban',
+        price: '$200',
+        img: './assets/img/Ray - Ban/ORIGINAL WAYFARER COLORBLOCK TRANSITIONS/1.png',
+        type:'Sun Glasses'
+    },
+    {
+        id: 2,
+        name: 'Booster',
+        page:'./product-detail/product02.html',
+        desc: 'Composed of a black acetate frame front and thin gold metal temples with grooved details, Booster 01 features an updated rendition of the Wellington style highlighted by a keyhole bridge and gold metal linings within the rims. This frame is finished with detailed endpieces, titanium temples, and Zeiss lenses with blue light protection & total UV protection.',
+        brand: 'Gentle Monster',
+        price: '$289',
+        img: './assets/img/Gentle Monster/Booster/2.png',
+        type:'Sun Glasses'
+    },
+    {
+        id: 3,
+        name: 'Jennie - Cloudy Day',
+        page:'./product-detail/product03.html',
+        desc: 'Jentle Garden introduces Cloudy Day Only 031, a gold metal round frame that is slightly wider at the top for a more flattering look and features an unique bridge design. The black marble temple tips add sophistication to the look. Fitted with clear blue light filtering lenses and 99.9% UV protecting Zeiss Lenses. Each frame is crafted by hand and marble-effect design may differ between products.',
+        brand: 'Gentle Monster',
+        price: '$269',
+        img: './assets/img/Gentle Monster/Jennie - Cloudy Day/2.png',
+        type:'Sun Glasses'
+    },
+    {
+        id: 4,
+        name: 'S.O.A',
+        page:'./product-detail/product04.html',
+        desc: 'S.O.A G1 is a rectangular grey combination frame. The acetate and titanium are crafted together to create a polished and refined lightweight design. This frame features a multi-layered rim and grooved endpieces along with 99.9% UV protected blue lenses.',
+        brand: 'Gentle Monster',
+        price: '$380',
+        img: './assets/img/Gentle Monster/S.O.A/2.png',
+        type:'Sun Glasses'
+    },
+    {
+        id: 5,
+        name: 'Una C',
+        page:'./product-detail/product05.html',
+        desc: 'The Una C B4 is an oversized fully acetate frame in brown with double metal studs in gold at the frame front and our logo piece in gold on the temples. The pattern for each product will vary due to the nature of the acetate production.',
+        brand: 'Gentle Monster',
+        price: '$190',
+        img: './assets/img/Gentle Monster/Una C/2.png',
+        type:'Sun Glasses'
+    },
+    {
+        id: 6,
+        name: 'Dark Chrome Symbole Sunglasses',
+        page:'./product-detail/product06.html',
+        desc: 'Metal sunglasses characterized by the contemporary touch of the flat geometric front combined with straight temples. The double bridge is accented with a metal insert that reinterprets the historic triangle logo of the maison.',
+        brand: 'Prada',
+        price: '$250',
+        img: './assets/img/Prada/Dark Chrome Symbole sunglasses/3.png',
+        type:'Sun Glasses'
+    },
+    {
+        id: 7,
+        name: 'Eyewear Collection',
+        page:'./product-detail/product07.html',
+        desc: 'Metal sunglasses with refined acetate trim that accents the modern geometric lenses. The sleek yet distinctive double bridge reflects contemporary appeal. Prada lettering logo.',
+        brand: 'Prada',
+        price: '$250',
+        img: './assets/img/Prada/Eyewear Collection sunglasses/3.png',
+        type:'Sun Glasses'
+    },
+    {
+        id: 8,
+        name: 'Runway sunglasses',
+        page:'./product-detail/product08.html',
+        desc: 'These acetate sunglasses with a rectangular shape have a contemporary frame front characterized by the three-dimensional workmanship and thick lines. The bridge area iconographically reinterprets the historic triangle logo of the fashion house. The wide straight temples are decorated with the Prada lettering logo.',
+        brand: 'Prada',
+        price: '$200',
+        img: './assets/img/Prada/Runway sunglasses/3.png',
+        type:'Sun Glasses'
+    },
+    {
+        id: 9,
+        name: 'AVIATOR CLASSIC',
+        page:'./product-detail/product09.html',
+        desc: 'These acetate sunglasses with a pilot silhouette are characterized by their bold lines and Havana nuances. The geometric temples feature an iconographic interpretation of Prada\'s traditional triangle logo in a contrasting color.',
+        brand: 'Ray Ban',
+        price: '$230',
+        img: './assets/img/Ray - Ban/AVIATOR CLASSIC/1.png',
+        type:'Sun Glasses'
+    },
+    {
+        id: 10,
+        name: 'JUSTIN CLASSIC',
+        page:'./product-detail/product10.html',
+        desc: 'Ray-Ban Justin sunglasses may just be one of the coolest looks in the Ray-Ban collection. Inspired by the iconic Wayfarer sunglasses, this style has been redesigned with slightly larger rectangle lenses, fresh new frame colors, stunning colored lenses and a rubberized frame finish to really give it that extra edge. Justin RB4165 sunglasses are bold, fun and unforgettable. These sunglasses come in traditional frame colors such as black, grey, brown and tortoise, as well as some total statement-making colors such as red, violet, blue, fuchsia, turquoise and matte blue. Add some edge to your style with these durable and unmistakable, iconic sunglasses.',
+        brand: 'Ray Ban',
+        price: '$155',
+        img: './assets/img/Ray - Ban/JUSTIN CLASSIC/1.png',
+        type:'Sun Glasses'
+    },
+    {
+        id: 11,
+        name: 'ORIGINAL WAYFARER CLASSIC',
+        page:'./product-detail/product11.html',
+        desc: 'Ray-Ban Original Wayfarer Classics are the most recognizable style in the history of sunglasses. Since its initial design in 1952, Wayfarer Classics gained popularity among celebrities, musicians, artists and those with an impeccable fashion sense. <br><br> As an iconic style of sunglasses, Original Wayfarer Classics always make a statement. Choose a classic pair of black-framed Wayfarer sunglasses from a variety of lens treatments including crystal green, crystal grey gradient, G-15 polarized and more.',
+        brand: 'Ray Ban',
+        price: '$163',
+        img: './assets/img/Ray - Ban/ORIGINAL WAYFARER CLASSIC LOW BRIDGE FIT/1.png',
+        type:'Sun Glasses'
+    },
+    {
+        id: 12,
+        name: 'ColorBlends Monthly',
+        page:'./product-detail/product12.html',
+        desc: 'FreshLook Colorblends are monthly coloured contact lenses by Alcon & Ciba Vision. Whether you\'re after a subtle eye colour update or are wanting to turn heads with a more dramatic change, achieve your desired look with these exceptional coloured lenses. <br><br> FreshLook Colorblends utilise 3-in-1 colour blend technology for a natural looking colour. It innovatively blends three different shades together and mimics the varying tones of your iris. <br><br> The FreshLook Colorblends range comes in 12 colours. Grey, Green or Brown provide a gentle enhancement for light coloured eyes, while Blue, Pure Hazel and Honey are a great choice of FreshLook Colorblends contacts for dark brown eyes. For those after an entirely fresh look, Brilliant Blue, Gemstone Green, True Sapphire, Sterling Grey or Amethyst offer a more striking update. <br><br> If you want contact lenses purely for cosmetic use, these high-quality coloured contacts are available in plano \(zero prescription form\). Even if it\'s for cosmetic purposes, you should check with your eye care professional to see if FreshLook Colorblends are suitable for you. <br><br> FreshLook Colorblends contact lenses deliver an unparalleled performance, freshness and comfort, that doesn\'t change throughout the day. Made with a 55% water content, these lenses hydrate your eyes over the course of the day. FreshLook Colorblends prevent the onset of dry eyes and keep them moist all day long. As a result, clear and comfortable vision is delivered from the start of the day to the end of it with these high-quality monthly contact lenses',
+        brand: 'Freshlook',
+        price: '$110',
+        img: './assets/img/Freshlook/5.png',
+        type:'Contact Lenses'
+    }
+]
+
+function render(dataSelect=[], typeSelect = []) {
+    var listProduct = document.getElementById("listProduct");
+    listProduct.innerHTML = '';
+    for(i=0; i<products.length; i++){
+        nameArr = products[i].name
+        pageArr = products[i].page
+        descArr = products[i].desc
+        brandArr = products[i].brand
+        priceArr = products[i].price
+        imgArr = products[i].img
+        typeArr = products[i].type
+
+        if(dataSelect.length > 0) {
+            if(dataSelect.includes(brandArr)== false ) continue;
+        }
+
+        if(typeSelect.length > 0) {
+            if(typeSelect.includes(typeArr)== false ) continue;
+        }
+
+        listProduct.innerHTML += `
+        <div class="col-lg-4">
+            <a href="${pageArr}" class="product-link">
+                <div class="img-contain">
+                    <img class="product-img" src="${imgArr}" alt="${nameArr}">
+                </div>
+                <div class="product-cover">
+                    <h3 class="product-name">${nameArr}</h3>
+                    <div class="product-detail">
+                        <div class="product-brand">${brandArr}</div>
+                        <div class="product-price">${priceArr}</div>
+                    </div>
+                </div>
+            </a>
+        </div> 
+        `;
+    }
+}
+render();
+
+function sort() {
+    var data = document.getElementsByClassName("sort__brand");
+    var dataSelect = [];
+    for(i=0; i<data.length; i++) {
+        if(data[i].checked) dataSelect.push( data[i].value );
+    }
+
+    var type = document.getElementsByClassName("sort__type");
+    var typeSelect = [];
+    for(i=0; i<type.length; i++) {
+        if(type[i].checked) typeSelect.push( type[i].value );
+    }
+    render(dataSelect, typeSelect);
+}
+
